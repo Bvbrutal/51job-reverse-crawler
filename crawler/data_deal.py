c37ef6758045b6e6783a754b23e033ddd84ed1e5
pengsonghuan <1023092893@qq.com>
2023-08-31 14:13:55 +0800

    :sparkles:fix:处理薪资格式问题和空值填充

--- crawler/data_deal.py
+++ crawler/data_deal.py
@@ -4,21 +4,28 @@ from datetime import datetime
 
 def data_deal():
     global x, y
-    df = pd.read_csv('static/data.csv', header=None, encoding='utf-8-sig')
+    df = pd.read_csv('static/data_undeal.csv', header=None, encoding='utf-8-sig')
     temp = pd.to_datetime(df[10])
     df[10] = temp.apply(lambda x: datetime.strftime(x, "%Y-%m-%d"))
     df = df.replace('[(（].*[）)]', '', regex=True)
-    df.drop_duplicates(keep='first', inplace=True)# 删除重复值
+    df.drop_duplicates(keep='first', inplace=True)  # 删除重复值
     df.reset_index(drop=True, inplace=True)
     for i in range(0, df.shape[0]):  # 0-1349 数据条数a
         try:
             s = df.loc[[i], [6]].values.tolist()[0][0]
             if s[-3:-1] == '下/':
-                df.loc[[i], [6]] = int(s[0]) * 10000 / 12
+                df.loc[[i], [6]] = int(eval(s[0]) * 10000 / 12)
                 continue
             if s[-1] == '天':
-                df.loc[[i], [6]] = int(s[0:-3]) * 22
-                continue
+                if s[-3] == '千':
+                    df.loc[[i], [6]] = int(eval(s[0:-3]) * 1000 * 22)
+                    continue
+                elif s[-3] == '万':
+                    df.loc[[i], [6]] = int(eval(s[0:-3]) * 10000 * 22)
+                    continue
+                else:
+                    df.loc[[i], [6]] = int(eval(s[0:-3]) * 22)
+                    continue
             if str(s) == 'nan':
                 continue
             if str(s)[-1] == '下':
@@ -61,18 +68,22 @@ def data_deal():
                         x = eval(a[0:]) * 10000
                         y = eval(b[0:-1]) * 10000
                     else:
+                        print(s)
                         continue
                 else:
+                    print(s)
                     continue
                 value = round((x + y) / 2)
                 df.loc[[i], [6]] = value
         except:
             continue
+    # nan字段填充
     df[6].fillna(df[6].mean(), inplace=True)
     df[2].fillna('无需学历', inplace=True)
+    df[3].fillna('其他', inplace=True)
+    df[4].fillna('暂无', inplace=True)
+    # 薪资数据转换
     df = df.astype({6: 'int'})
-    df1 = df[df[4].notnull()]
-    df1.reset_index(drop=True, inplace=True)
     df.reset_index(drop=True, inplace=True)
     for i in range(df.shape[0]):
         df[7][i] = df[7][i].replace("'", '')
